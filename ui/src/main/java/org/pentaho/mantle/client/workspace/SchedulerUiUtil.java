b04d17040617d9545131a22b93dc58d8044e7353
William Hartman <william.hartman@hitachivantara.com>
2024-03-14 15:43:43 -0400

    [CLEANUP] Add copyright headers, whitespace fixes

--- ui/src/main/java/org/pentaho/mantle/client/workspace/SchedulerUiUtil.java
+++ ui/src/main/java/org/pentaho/mantle/client/workspace/SchedulerUiUtil.java
@@ -1,3 +1,19 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2024 Hitachi Vantara. All rights reserved.
+ */
 package org.pentaho.mantle.client.workspace;
 
 import com.google.gwt.core.client.JsArray;
@@ -57,7 +73,7 @@ public class SchedulerUiUtil {
     HashSet<String> visitedParamNames = new HashSet<>();
     for ( int i = 0; i < paramsRaw.length(); i++ ) {
       JsJobParam param = paramsRaw.get( i );
-      if ( !toFilter.contains( param.getName() ) && !visitedParamNames.contains( param.getName() )) {
+      if ( !toFilter.contains( param.getName() ) && !visitedParamNames.contains( param.getName() ) ) {
         params.add( paramsRaw.get( i ) );
         visitedParamNames.add( param.getName() );
       }
