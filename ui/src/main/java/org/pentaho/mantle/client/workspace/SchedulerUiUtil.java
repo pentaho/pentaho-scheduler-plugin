1b8a8a2ce3998ae170ae7fea6857218a6f7941a2
srallapa <srinivas.rallapalli@hitachivantara.com>
2024-12-09 14:54:14 -0600

    [BISERVER-15176] reverting the changes from BISERVER-15051

--- ui/src/main/java/org/pentaho/mantle/client/workspace/SchedulerUiUtil.java
+++ ui/src/main/java/org/pentaho/mantle/client/workspace/SchedulerUiUtil.java
@@ -68,7 +68,7 @@ public class SchedulerUiUtil {
   includes duplicated values, so we remove those too. Also converts the params array into
   a more convenient list type.
    */
-  public static List<JsJobParam> getFilteredJobParams( JsJob job, final boolean hideInternalVariables ) {
+  public static List<JsJobParam> getFilteredJobParams( JsJob job ) {
     JsArray<JsJobParam> paramsRaw = job.getJobParams();
     ArrayList<JsJobParam> params = new ArrayList<>();
 
@@ -81,14 +81,8 @@ public class SchedulerUiUtil {
       JsJobParam param = paramsRaw.get( i );
 
       if ( !toFilter.contains( param.getName() ) && !visitedParamNames.contains( param.getName() ) ) {
-        if( hideInternalVariables ){
-          if( !paramsRaw.get(i).getName().contains("Internal.") ) {
-            params.add(paramsRaw.get(i));
-          }
-        } else {
-          params.add(paramsRaw.get(i));
-        }
-        visitedParamNames.add(param.getName());
+        params.add( paramsRaw.get( i ) );
+        visitedParamNames.add( param.getName() );
       }
     }
 
